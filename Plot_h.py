Fixed a sign

Plot_h.py
@@ -42,20 +42,20 @@ def ImportData_ED(physical_data, project_name = ""):
 
     return all, disc
 
-beta_array = [0.2, 0.6, 0.8, 1.0]
+beta_array = [0.2]
 
 for beta in beta_array:
 
-    all_1, times = ImportData(f"ISO__Square_NN_PBC_N=20__beta={beta:.2g}__h_z=2__rescale=0.5")
+    all_1, times = ImportData(f"Mag_Field/ISO__Square_NN_PBC_N=24__beta={beta:.2g}__h_z=2__rescale=-0.5")
 
-    G_1 = np.array( [ gab for gab in all_1['results']['Re_correlation']][0] )
+    G_1 = np.array( [ gab for gab in all_1['results']['Im_correlation']][1] )
     # print(np.abs(G_ed[-1]-G_1[-1]))
-    G_mirror = np.concatenate((G_1,np.flip(G_1)))
-    plt.plot(times, G_mirror, label=rf'CET N=20, $\beta={beta:.2g}$')
+    G_1 = np.concatenate((G_1,-np.flip(G_1)))
+    plt.plot(times, G_1, label=rf'CET N=24, $\beta={beta:.2g}$')
 
 plt.xlabel(r'$\tau$/$\beta$')
 plt.ylabel(r'$g_{xx}$($\tau$)')
 # plt.ylim(0.24, 0.26)
 plt.legend(fontsize=8)
 
-plt.savefig("Plots/g_xx_h.pdf")
+plt.savefig("Plots/Test_h.pdf")

Plot_site.py
@@ -44,13 +44,13 @@ def ImportData_ED(physical_data, project_name = ""):
 
     return all, disc
 
-site_array = [0, 1, 2, 6]
+site_array = [0, 1, 2, 7]
 
 sqsums = np.array([])
 
 for site in site_array:
 
-    all_1, times = ImportData(f"ISO__Square_NN_PBC_N=20__site={site}__beta=0__rescale=0.5", project_name="Real_Time")
+    all_1, times = ImportData(f"ISO__Square_NN_PBC_N=24__site={site}__beta=0__rescale=0.5", project_name="Real_Time")
 
     G = np.array( [ gab for gab in all_1['results']['Re_correlation']][0] )
     plt.plot(times, G, label = rf'Chebyshev, site={site}')
@@ -62,7 +62,7 @@ plt.ylabel(r'$g_{xx}$($t$)')
 # plt.ylim(0.24, 0.26)
 plt.legend()
 
-plt.savefig("Plots/Test.pdf")
+plt.savefig("Plots/Test_24.pdf")
 plt.clf()
 # plt.plot(beta_array, sqsums, 'o')
 # plt.yscale('log')