9db89068dd6835ac2ed2c3cb40cf5d69b8fe861b
przembien00 <przemyslaw.bieniek@tu-dortmund.de>
2025-10-07 17:13:45 +0200

    Random couplings

--- make_couplings_random.py
+++ make_couplings_random.py
@@ -3,19 +3,17 @@ import h5py as h5
 import matplotlib.pyplot as plt
 
 J = 1
-N = 16
+N = 20
 
-couplings = np.random.normal(0, J/N**0.5, (N, N))  # Random couplings
+couplings = np.random.uniform(0.9, 1.1, (N, N))  # Random couplings
 couplings = np.triu(couplings, 1) + np.triu(couplings, 1).T  # Make it symmetric
 couplings -= np.diag(np.diag(couplings))  # Set diagonal to zero
 print(couplings)
 JQ = np.sum(couplings**2, axis=0)[0]**0.5
+couplings *= J / JQ  # Normalize
 print(JQ)
-couplings /= JQ  # Normalize
-print(couplings)
-print(np.sum(couplings**2, axis=0)[0]**0.5)  # Check normalization
 # Save as HDF5
-with h5.File(f"Couplings/Square_Glass_N={N}.hdf5", "w") as f:
+with h5.File(f"Couplings/Square_Random_N={N}.hdf5", "w") as f:
     all = f.create_group("all")
     all.create_dataset("J_ij", data=couplings, dtype='float64')
     all.attrs["num_Spins"] = N
